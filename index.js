6b9aec89b72954393539cb4c80b78c0492fb67f8
rnrabi <rnrabi913@gmail.com>
2024-05-11 14:37:42 +0600

    get api accroding to id

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/update/:id' , async(req, res)=>{
+            const id = req.params.id;
+            const query= {_id: new ObjectId(id)};
+            const result = await foodsCollection.findOne(query);
+            res.send(result)
+        })
+
 
         app.post('/users', async (req, res) => {
             const user = req.body;
