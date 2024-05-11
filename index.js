b588442201c364271949f29cd95fbfcfd5286e93
rnrabi <rnrabi913@gmail.com>
2024-05-11 19:41:10 +0600

    details api ok

--- index.js
+++ index.js
@@ -53,6 +53,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/detail/:id' , async(req, res)=>{
+            const id = req.params.id;
+            const query= {_id: new ObjectId(id)};
+            const result = await foodsCollection.findOne(query);
+            res.send(result)
+        })
+
         app.put('/update/:id' , async(req, res)=>{
             const id = req.params.id;
             const info = req.body;
